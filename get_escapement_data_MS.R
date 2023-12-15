a4b2efc4510c86fa40a1146eb8533fe6c1e30880
Mark Sorel <mark.sorel@dfw.wa.gov>
2023-12-15 09:16:54 -0800

    adds a bunch of interpolaation techniques that I explored but plan to delete

--- get_escapement_data_MS.R
+++ get_escapement_data_MS.R
@@ -133,25 +133,40 @@ all.esc.df<-bind_rows(wa.esc.df2, or.esc.df,willamette.upper.esc %>% mutate(sour
 
 #add tributary harvest rates (table of values originating from god knows where)
 new.df<-all.esc.df %>% left_join(trib.mort.rates) %>% 
-  # filter(Year>=2001) %>% #years with more complete data
+  filter(Year>=2001) %>% #years with more complete data
 #expand escapement to account for tributary harvest
-  mutate(Population_Escapement=Population_Escapement/(1-TribMortRate))
+  mutate(Population_Escapement=Population_Escapement/(1-TribMortRate)) %>% filter(Population_Name!="Lower Gorge (Columbia)")
 
 
-new.df %>% group_by(Population_Name,source) %>% 
+new.df<-all.esc.df %>% left_join(trib.mort.rates) %>% 
+  # filter(between(Year,2001,2022)) %>% #years with more complete data
+  filter(Year<=2022) %>%
+  #expand escapement to account for tributary harvest
+  mutate(Population_Escapement=Population_Escapement/(1-TribMortRate)) %>% filter(Population_Name!="Lower Gorge (Columbia)")
+
+
+new.df  %>% group_by(Population_Name,source) %>% 
   summarize(min_year=min(Year),
             max_year=max(Year),
+            missing_years=sum(is.na(Population_Escapement)),
             n_years=n(),
             mean_esc=round(mean(Population_Escapement,na.rm=T),2)) %>%View()# write_csv(file="winter_stlhd.csv")
 
-
-
+#plot data time series by population
+ ggplot(new.df,aes(x=Year,y=Population_Escapement))+geom_line()+geom_point()+facet_wrap(~Population_Name,scales="free_y")
+ 
 #pivot wider with column for each population for MARSS modeling
 wide_df.ts<-new.df %>% 
-  filter(Population_Name%in%c("Clackamas River","Kalama","Sandy River")) %>% 
+ 
+    arrange(DPS_ESU,Population_Name) %>% 
+  ##ALERT!!! filtering to only three populations for model testing.
+  # filter(Population_Name%in%c("Clackamas River","Kalama","Sandy River")) %>% 
   dplyr::select(Year,Population_Name,Population_Escapement) %>% pivot_wider(names_from = Population_Name,values_from = Population_Escapement) %>% arrange(Year) %>% 
   #drop Year column and log for input into MARSS
   select(-1) %>% log
+
+
+
 
 #look at correlations between population escapement time series
 cor.ts<-cor(wide_df.ts,use="pairwise.complete.obs")
@@ -159,47 +174,193 @@ cor.ts<-cor(wide_df.ts,use="pairwise.complete.obs")
 
 # Z-score escapements
 test.ts_z<-scale(wide_df.ts)
-att_Z<-attributes(test.ts_z)#save means and SDs for back-transform
+att_Z<-attributes(test.ts_z) #save means and SDs for back-transform
 
-#fitt MARSS model to log escapement
-## I explored some different specifications and the below one had most support based on AICc
+DPS_vec<-new.df$DPS_ESU[match(colnames(test.ts_z),new.df$Population_Name)]
 
+
+#fit MARSS model to log escapement
+
+ns <- ncol(wide_df.ts)
+B <- "diagonal and equal"
+Q <- "unconstrained"
+R <- diag(0.01, ns)
+U <- "zero"
+A <- "zero"
+x0 <- "unequal"
+mod.list.ar1 = list(B = B, Q = Q, R = R, U = U, x0 = x0, A = A, 
+                    tinitx = 1)
+
+
+
+library(MARSS)
+# m <- apply(test.ts_z, 2, mean, na.rm = TRUE)
+fit.ar1 <- MARSS(t(test.ts_z), model = mod.list.ar1, control = list(maxit = 5000),method="BFGS")
+
+
+
+
+fit <- fit.ar1
+d <- fitted(fit, interval = "prediction", type = "ytT")
+d$Year <- d$t + 1980
+d$Station <- d$.rownames
+p <- ggplot(data = d) + geom_line(aes(Year, .fitted)) + geom_point(aes(Year, 
+                                                                       y)) + geom_ribbon(aes(x = Year, ymin = .lwr, ymax = .upr), 
+                                                                                         linetype = 2, alpha = 0.2, fill = "blue") + facet_wrap(~Station) + 
+  xlab("") + ylab("SWE (demeaned)")
+p
+
+
+
+
+
+cnts<-d  %>% select(Year,.fitted,Station) %>% pivot_wider(values_from=.fitted,names_from   = Station) %>% arrange(Year) %>% select(-Year)
+
+exp(cnts*att_Z$`scaled:scale`+att_Z$`scaled:center`) %>% rowSums() %>% `names<-` (new.df$Year %>% unique %>% sort)
+
+
+#residuals
+fit <- fit.ar1
+par(mfrow = c(5, 5), mar = c(2, 2, 1, 1))
+apply(MARSSresiduals(fit, type = "tt1")$model.residuals, 
+      1, acf, na.action = na.pass)
+
+#-----------------------------------------------
+### correlation only
+ns <- length(unique(swe.feb$Station))
+B <- "zero"
+Q <- "unconstrained"
+R <- diag(0.01, ns)
+U <- "zero"
+A <- "zero"
+x0 <- "zero"
+mod.list.corr = list(B = B, Q = Q, R = R, U = U, x0 = x0, A = A, 
+                     tinitx = 0)
+
+fit.corr <- MARSS(t(test.ts_z), model = mod.list.corr)
+
+fit <- fit.corr
+d <- fitted(fit, type = "ytT", interval = "prediction")
+d$Year <- d$t + 2022-max(d$t)
+d$Station <- d$.rownames
+p <- ggplot(data = d) + geom_line(aes(Year, .fitted)) + geom_point(aes(Year, 
+                                                                       y)) + geom_ribbon(aes(x = Year, ymin = .lwr, ymax = .upr), 
+                                                                                         linetype = 2, alpha = 0.2, fill = "blue") + facet_wrap(~Station) + 
+  xlab("") + ylab("SWE (demeaned)")
+p
+
+cnts<-d  %>% select(Year,.fitted,Station) %>% pivot_wider(values_from=.fitted,names_from   = Station) %>% arrange(Year) %>% select(-Year)
+
+exp(cnts*att_Z$`scaled:scale`+att_Z$`scaled:center`) %>% rowSums() %>% `names<-` (new.df$Year %>% unique %>% sort)
+
+
+
+#-----------------------------------------------
+### DFA
+
+n_fac<-3
+
+B <- matrix(list(0), n_fac, n_fac)
+diag(B)<-paste0("b",1:n_fac)
+
+
+Q <- diag(1, n_fac)
+R <- "diagonal and unequal"
+U <- "zero"
+x0 <- "zero"
+Z <- matrix(list(0), ns, n_fac)
+Z[1:(ns * n_fac)] <- paste0(rep(paste0("z",1:n_fac),each=ns),rep(1:ns,times=n_fac))
+
+Z[upper.tri(Z)]<-0
+A <- "zero"
+mod.list.dfa = list(B = B, Z = Z, Q = Q, R = R, U = U, A = A, 
+                    x0 = x0)
+
+
+
+fit.dfa <- MARSS(t(test.ts_z), model = mod.list.dfa, control = list(maxit = 1000),
+                 inits = list(A = matrix(0, ns, 1)))
+
+fit.dfa$AICc
+
+fit <- fit.dfa
+d <- fitted(fit, type = "ytT", interval = "prediction") %>% mutate(vals=ifelse(is.na(y),.fitted,y),interpolated=is.na(y))
+d$Year <- d$t+ 2022-max(d$t)
+d$Station <- d$.rownames
+p <- ggplot(data = d) + geom_line(aes(Year, vals)) + geom_point(aes(Year, 
+                                                                       vals,col=interpolated))  +
+
+  facet_wrap(~Station) + 
+  xlab("") + ylab("SWE (demeaned)")+ggplot2::scale_color_manual(values=c("black","red"))
+p
+
+cnts<-d  %>% select(Year,vals,Station) %>% pivot_wider(values_from=vals,names_from   = Station) %>% arrange(Year) %>% select(-Year)
+
+exp(cnts*att_Z$`scaled:scale`+att_Z$`scaled:center`) %>% rowSums() %>% `names<-` (new.df$Year %>% unique %>% sort) 
+
+
+#fit MARSS model to log escapement
+
+
+npop<-ncol(test.ts_z)
 #trend
-U=c("unequal","equal","zero")
+U_DPS<-matrix(DPS_vec,ncol=1)
+U=list("equal","zero",U_DPS=U_DPS)#,unequal,,U_DPS=U_DPS"equal","zero"
 #autocorrellation
-B=c("diagonal and equal","diagonal and unequal","identity")
+B_DPS<-matrix(list(0),npop,npop);diag(B_DPS)<-DPS_vec
+B=list("diagonal and equal","identity",B_DPS=B_DPS)#,"diagonal and unequal","zero"
+
+
 #process error
-Q=c( "diagonal and unequal", "diagonal and equal", "equalvarcov")
+Q_DPS_diag<-Q_DPS_cov<-B_DPS
+for ( i in 1:npop){
+  for(j in 1:npop){
+    if(i==j)next
+    if(DPS_vec[i]==DPS_vec[j]){
+      Q_DPS_cov[i,j]<-paste0(DPS_vec[i],"_cov")
+    }else{
+      next
+    }
+  }
+}
+
+test<-array(Q_DPS_cov,c(npop,npop,nrow(test.ts_z)))
+test[,,1]<-diag(npop)
+Q=list("diagonal and equal", "equalvarcov",Q_DPS_diag=Q_DPS_diag)#, " "diagonal and unequal","equalvarcov",
 #Observation error
-R=c("diagonal and equal","diagonal and unequal")
+R=c("diagonal and unequal","diagonal and equal")
+  # diag(ncol(test.ts_z))*.001
+  #c("diagonal and equal","diagonal and unequal")
 
 
-
+# 
+# install.packages("MARSS", repos = c("https://atsa-es.r-universe.dev", "https://cloud.r-project.org"))
 
 cntl.list <- list( maxit = 2000)
 
 out_list<-list()
 model.data <- data.frame(stringsAsFactors = FALSE)
 # fit models & store results
-for (b in B) {
-  for (q in Q) {
-    for(u in U){
-      for(r in R){
-      if(!(q=="zero" & b!="identity")){  
-        model <- list(R = r, B=b, Q=q, U = u)
+for (b in 1:length(B)) {
+  for (q in 1:length(Q)) {
+    for(u in 1:length(U)){
+      for(r in 1:length(R)){
+      # if(!(q=="zero" & b!="identity")){
+        model <- list(R = R[[r]], B=B[[b]], Q=Q[[q]], U = U[[u]])
         kemz <- MARSS::MARSS(t(test.ts_z),
-                             model = model, control = cntl.list)
+                             model = model)
         model.data <- rbind(
           model.data,
           data.frame(
-            R=r,
-            B = b,
-            Q = q,
-            U= u,
+            R=ifelse(length(R[[r]])>1,names(R)[r],R[[r]]),
+            B = ifelse(length(B[[b]])>1,names(B)[b],B[[b]]),
+            Q = ifelse(length(Q[[q]])>1,names(Q)[q],Q[[q]]),
+            U= ifelse(length(U[[u]])>1,names(U)[u],U[[u]]),
             logLik = kemz$logLik,
             K = kemz$num.params,
             AICc = kemz$AICc,
-            stringsAsFactors = FALSE
+            stringsAsFactors = FALSE,
+            preds=exp(kemz$ytT*att_Z$`scaled:scale`+att_Z$`scaled:center`) %>% colSums() %>% tail(1)
           )
         )
         assign(paste("kemz", b,q,u,r,sep = "."), kemz)
@@ -208,23 +369,110 @@ for (b in B) {
     } # end q loop
   } # end of U loop
 }
+# }
+
+model.data %>% arrange(AICc) %>% dplyr::mutate(delta_AICc=AICc-min(AICc),.after=AICc) %>% View
+
+model.data %>% arrange(AICc) %>% dplyr::mutate(delta_AICc=AICc-min(AICc),.after=AICc,preds=round(preds)) %>% write_csv("model.data.csv")
+
+#--------------------------------------------------------------
+
+#trend
+U_DPS<-matrix(DPS_vec,ncol=1)
+U=list(U_DPS=U_DPS)#,unequal,,U_DPS=U_DPS,"zero"
+#autocorrellation
+B_DPS<-matrix(list(0),npop,npop);diag(B_DPS)<-DPS_vec
+B=list("diagonal and equal")#,"diagonal and unequal","zero"
+
+
+#process error
+Q_DPS_diag<-Q_DPS_cov<-B_DPS
+for ( i in 1:npop){
+  for(j in 1:npop){
+    if(i==j)next
+    if(DPS_vec[i]==DPS_vec[j]){
+      Q_DPS_cov[i,j]<-paste0(DPS_vec[i],"_cov")
+    }else{
+      next
+    }
+  }
 }
 
-model.data %>% arrange(AICc)
+test<-array(Q_DPS_cov,c(npop,npop,nrow(test.ts_z)))
+test[,,1]<-diag(npop)
+Q=list( "diagonal and equal")#, "diagonal and unequal" "diagonal and unequal","equalvarcov",
+#Observation error
+R=c("diagonal and equal")
+
+
+
+for (b in 1:length(B)) {
+  for (q in 1:length(Q)) {
+    for(u in 1:length(U)){
+      for(r in 1:length(R)){
+        # if(!(q=="zero" & b!="identity")){  
+        model <- list(R = R[[r]], B=B[[b]], Q=Q[[q]], U = U[[u]])
+        kemz <- MARSS::MARSS(t(test.ts_z),
+                             model = model)
+        model.data <- rbind(
+          model.data,
+          data.frame(
+            R=ifelse(length(R[[r]])>1,names(R)[r],R[[r]]),
+            B = ifelse(length(B[[b]])>1,names(B)[b],B[[b]]),
+            Q = ifelse(length(Q[[q]])>1,names(Q)[q],Q[[q]]),
+            U= ifelse(length(U[[u]])>1,names(U)[u],U[[u]]),
+            logLik = kemz$logLik,
+            K = kemz$num.params,
+            AICc = kemz$AICc,
+            stringsAsFactors = FALSE,
+            preds=exp(kemz$ytT*att_Z$`scaled:scale`+att_Z$`scaled:center`) %>% colSums() %>% tail(1)
+          )
+        )
+        assign(paste("kemz", b,q,u,r,sep = "."), kemz)
+        out_list[[paste("kemz", b,q,u,r, sep = ".")]]<-kemz
+      } # end b loop
+    } # end q loop
+  } # end of U loop
+}
 
 
 
 
 
+library(mice)
+
+dimnames(test.ts_z)[[2]]<-NULL
+imputed_Data <- mice(test.ts_z, m=5, seed = 300)
+
+#compute estimates by year
+exp(complete(imputed_Data,1)*att_Z$`scaled:scale`+att_Z$`scaled:center`) %>% rowSums() %>% `names<-` (new.df$Year %>% unique %>% sort)
+
+exp(complete(imputed_Data,2)*att_Z$`scaled:scale`+att_Z$`scaled:center`) %>% rowSums() %>% `names<-` (new.df$Year %>% unique %>% sort)
+
+exp(complete(imputed_Data,3)*att_Z$`scaled:scale`+att_Z$`scaled:center`) %>% rowSums() %>% `names<-` (new.df$Year %>% unique %>% sort)
+
+library(Amelia)
+dim(test.ts_z)
+test.ts_z<-cbind(test.ts_z,year=1:nrow(test.ts_z))
+
+test<-amelia(x=test.ts_z,empri = 75,ts=24,poly=1)
+
+#compute estimates by year
+exp(test$imputations$imp1[,-24]*att_Z$`scaled:scale`+att_Z$`scaled:center`) %>% rowSums() %>% `names<-` (new.df$Year %>% unique %>% sort)
+
+
+
+#back calculated
+## latent states
+mod_states<-exp(fit1$states*att_Z$`scaled:scale`+att_Z$`scaled:center`)
+## observations with imputed values where data missing (using this for these values for the run reconstruction)
+mod_obs<- exp(fit1$ytT*att_Z$`scaled:scale`+att_Z$`scaled:center`) %>% colSums()
 
 
 
 
 
-
-
-
-
+#what Mark did in 2022
 
 
 library(MARSS)
