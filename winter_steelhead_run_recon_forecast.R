4d89161973aee254434ba7eb3612f1e4ff7887cf
Mark Sorel <mark.sorel@dfw.wa.gov>
2023-12-18 14:26:10 -0800

    add new script to call functions (get data and impute)

---
+++ winter_steelhead_run_recon_forecast.R
@@ -0,0 +1,39 @@
+wd_functions<-"functions"
+sapply(FUN = source, paste(wd_functions, list.files(wd_functions), sep="/"))
+
+#pull and munge data 
+dat<-get_data_fun(data_start_year = 0,data_end_year=2022)
+
+#plot data
+dat$ts_plot
+
+#look at data summary
+dat$data_summary
+
+# fit DFA with MARSS to interpolate (takes ~2 minutes)
+imputes<-impute_DFA_fun(dat$new.df)
+
+#plot of imputed values
+imputes$p2
+
+#plot of annual sums (including imputed values) with numbers of missing (i.e. imputed) values per year
+imputes$p3
+
+
+# join annual sum of counts with mainstem fishing mortality and sume to calculated river mouth run size
+RMR<-imputes$rollup %>% 
+  left_join(dat$mainstem.morts %>% group_by(Year) %>% summarize(morts=sum(Fishing_Mortality))) %>% 
+  mutate(runsize_obs=Counts +morts) 
+
+View(RMR)
+
+
+#calculated 5-year average river mouth run size
+RMR %>% tail(5) %>% summarize(mean(runsize_obs))
+
+#calculated 10-year average river mouth run size
+RMR %>% tail(10) %>% summarize(mean(runsize_obs))
+
+#fit ARIMA model to RMR time series
+fit<-forecast::auto.arima(RMR$runsize_obs)
+plot(forecast(fit,h=1))
